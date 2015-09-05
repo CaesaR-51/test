c7b9c0b671ad41d39cbfb5d7301dcacf22f20886
CaesaR-51 <artem.kuzmin@gmail.com>
2015-09-05 21:51:32 +0300

    2nd

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,13 @@
 // ConsoleApplication1.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 int main()
 {
+	cout << "Hello, world!";
     return 0;
 }
 
